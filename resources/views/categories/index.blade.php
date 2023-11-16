35eb40b08f31796719d25d25a8108c404d943295
Fukui1372 <kennosuke1372@icloud.com>
2023-11-16 13:10:34 +0900

    Add リレーション

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -15,15 +15,15 @@
             @Foreach($posts as $post)
                 <div class='post'>
                     <a href="/posts/{{ $post->id}}"><h2 class='title'>{{ $post->title }}</h2></a>
+                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a
                     <p class='body'>{{ $post->body }}</p>
                     <form action="/posts/{{ $post->id}}" id="form_{{ $post->id}}" method="post">
                     @csrf
                     @method('DELETE')
                     <button type="button" onclick="deletePost({{ $post->id}})">delete</button>
                 </form>
-                </div>
-                <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-            @endforeach
+          </div>
+            @endforeach>
         </div>
         <div class='paginate'>{{ $posts->links()}}</div>
     <script>
